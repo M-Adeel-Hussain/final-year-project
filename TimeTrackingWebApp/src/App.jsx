3afba6e3548f4872048fcef533001e179aecc65a
M-Adeel-Hussain <adeellinkind@gmail.com>
2025-06-05 11:46:15 +0500

    Final Year Project

--- TimeTrackingWebApp/src/App.jsx
+++ TimeTrackingWebApp/src/App.jsx
@@ -20,7 +20,7 @@ function Rout() {
       <>
       <BrowserRouter basename='final-year-project'>
         <Routes>
-      <Route exact path="/final-year-project" element={<Start/>} />
+      <Route exact path="/" element={<Start/>} />
       <Route exact path="/orgDashboard" element={isAuthenticated ?<Org /> : <OrgLogin />}/>
         <Route exact path="/orgLogin" element={<OrgLogin />} />
         <Route exact path="/orgReg" element={<OrgReg />} />
